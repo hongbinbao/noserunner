6070afdc8b7ecfdb1fb3764e0889ad44adbb90db
hongbinbao <hongbin.bao@gmail.com>
2014-03-07 15:09:27 +0800

    create zip file in memory before uploading

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import requests
+import StringIO
+import zipfile
 from uuid import uuid1
 from ConfigParser import ConfigParser
 import json, hashlib, math, time, threading, sys, os
@@ -92,6 +94,54 @@ def getContentType(filename):
     if ext == 'zip': return 'application/zip'
     return None
 
+class MemoryZip(object):
+ 
+    def __init__(self):
+        # Create the a memory file-like object
+        self.memory_zip = StringIO.StringIO()
+ 
+    def appendFile(self, file_path, file_name=None):
+        '''
+        read local source file and add into memory
+        '''
+        if file_name is None:
+            p, fn = os.path.split(file_path)
+        else:
+            fn = file_name
+        c = open(file_path, "rb").read()
+        self.append(fn, c)
+        return self
+ 
+    def append(self, filename_in_zip, file_contents):
+        '''
+        Appends a file with name filename_in_zip and contents of
+        file_contents to the in-memory zip
+        '''
+        # Get a handle to the in-memory zip in append mode
+        zf = zipfile.ZipFile(self.memory_zip, "a", zipfile.ZIP_DEFLATED, False)
+        # Write the file to the in-memory zip
+        zf.writestr(filename_in_zip, file_contents)
+        # Mark the files as having been created on Windows so that
+        # Unix permissions are not inferred as 0000
+        for zfile in zf.filelist:
+            zfile.create_system = 0
+        return self
+ 
+    def read(self):
+        '''
+        Returns a string with the contents of the in-memory zip
+        '''
+        self.memory_zip.seek(0)
+        return self.memory_zip.read()
+ 
+    def writetofile(self, filename):
+        '''
+        Writes the in-memory zip to a file.
+        '''
+        f = file(filename, "wb")
+        f.write(self.read())
+        f.close()
+ 
 class Authentication(object):
 
     @staticmethod
