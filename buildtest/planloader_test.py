6fbc874a0edd13ff6efce9b1f3a2dccdb68fcde4
hongbinbao <hongbin.bao@gmail.com>
2014-06-12 17:46:46 +0800

    update plan loader test case

--- buildtest/planloader_test.py
+++ buildtest/planloader_test.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+
 import sys
 import unittest
 from os.path import join, dirname
 sys.path.append('..')
 from noserunner import planloader
+
+
 class PlanLoaderTest(unittest.TestCase):
     def setUp(self):
         self.m = getattr(planloader, 'readTestsFromConfigFile')
         self.plan = join(join(dirname(__file__),'resource'),'plan')
         self.repeat_plan = join(join(dirname(__file__),'resource'),'repeatplan')
 
-    def testNormalPlan(self):
+    def testLoadNormalPlan(self):
         tests = self.m(self.plan, section='tests')
         self.assertEqual(tests[0][0] ,'sample.testcases.phone.Phone.testCall')
         self.assertEqual(tests[0][1] , 2)
         self.assertEqual(tests[1][0] ,'sample.testcases.browser.Browser.testOpenBrowser')
         self.assertEqual(tests[1][1] , 3)
 
-    def testRepeatPlan(self):
+    def testLoadRepeatPlan(self):
         tests = self.m(self.repeat_plan, section='tests')
         self.assertEqual(tests[0][0] , 'sample.testcases.phone.Phone.testCall')
         self.assertEqual(tests[0][1] , 2)
